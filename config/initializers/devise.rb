45f0769b113e0d25097668744393ac31e1a820dd
shivani146 <shivani.sep.gupta@gmail.com>
2018-05-20 13:17:19 +0530

    added ui to login page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -132,6 +132,7 @@ Devise.setup do |config|
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
   # config.confirm_within = 3.days
+  config.scoped_views = true
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
